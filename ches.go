54d764dcf5b5ecf5f80d98cdb72d6bd87860bf42
Geomelon <geomelon@yandex.ru>
2025-11-17 00:32:27 +0300

    made a refresh every minute

--- ches.go
+++ ches.go
@@ -3,51 +3,63 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"time"
 
 	"github.com/gocolly/colly"
 )
 
 func main() {
-	emptyServer := true
-	babydoll := colly.NewCollector()
-	babydoll.UserAgent = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36")
+	for {
+		clearScreen()
 
-	babydoll.OnHTML(".user-server b", func(userLink *colly.HTMLElement) {
-		fmt.Println(" ", userLink.Text)
-		emptyServer = false
-	})
+		emptyServer := true
+		babydoll := colly.NewCollector()
+		babydoll.UserAgent = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36")
 
-	fmt.Println("Players Online:")
-	fmt.Println("\nPOWER TECH")
-	babydoll.Visit("https://letragon.ru/servers/pt/")
-	if emptyServer {
-		fmt.Println("---пусто---")
+		babydoll.OnHTML(".user-server b", func(userLink *colly.HTMLElement) {
+			fmt.Println(" ", userLink.Text)
+			emptyServer = false
+		})
+
+		fmt.Println("Players Online:")
+		fmt.Println("\nPOWER TECH")
+		babydoll.Visit("https://letragon.ru/servers/pt/")
+		if emptyServer {
+			fmt.Println("---пусто---")
+		}
+		emptyServer = true
+		fmt.Println("\nHEAVY TECH")
+		babydoll.Visit("https://letragon.ru/servers/ht/")
+		if emptyServer {
+			fmt.Println("---пусто---")
+		}
+		emptyServer = true
+		fmt.Println("\nMAGIC")
+		babydoll.Visit("https://letragon.ru/servers/m/")
+		if emptyServer {
+			fmt.Println("---пусто---")
+		}
+		emptyServer = true
+		fmt.Println("\nMISUSE TECH")
+		babydoll.Visit("https://letragon.ru/servers/mt/")
+		if emptyServer {
+			fmt.Println("---пусто---")
+		}
+		emptyServer = true
+		fmt.Println("\nNEVER CHANGE")
+		babydoll.Visit("https://letragon.ru/servers/nc/")
+		if emptyServer {
+			fmt.Println("---пусто---")
+		}
+
+		time.Sleep(60 * time.Second)
 	}
-	emptyServer = true
-	fmt.Println("\nHEAVY TECH")
-	babydoll.Visit("https://letragon.ru/servers/ht/")
-	if emptyServer {
-		fmt.Println("---пусто---")
-	}
-	emptyServer = true
-	fmt.Println("\nMAGIC")
-	babydoll.Visit("https://letragon.ru/servers/m/")
-	if emptyServer {
-		fmt.Println("---пусто---")
-	}
-	emptyServer = true
-	fmt.Println("\nMISUSE TECH")
-	babydoll.Visit("https://letragon.ru/servers/mt/")
-	if emptyServer {
-		fmt.Println("---пусто---")
-	}
-	emptyServer = true
-	fmt.Println("\nNEVER CHANGE")
-	babydoll.Visit("https://letragon.ru/servers/nc/")
-	if emptyServer {
-		fmt.Println("---пусто---")
-	}
-	emptyServer = true
-	var n string
-	fmt.Scan(&n)
+}
+
+func clearScreen() {
+	command := exec.Command("cmd", "/c", "cls")
+	command.Stdout = os.Stdout
+	command.Run()
 }
