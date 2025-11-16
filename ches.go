dd77f562061ac802f00f6fc52556594825afd653
Geomelon <geomelon@yandex.ru>
2025-11-16 22:30:46 +0300

    output players of one server

--- ches.go
+++ ches.go
@@ -13,9 +13,10 @@ func main() {
 	babydoll.UserAgent = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36")
 
 	// var users []string
-	babydoll.OnHTML(".user-server", func(userLink *colly.HTMLElement) {
-		fmt.Println(" Aboba ", userLink.Text)
+	babydoll.OnHTML(".user-server b", func(userLink *colly.HTMLElement) {
+		fmt.Println(userLink.Text)
 	})
 
+	fmt.Println("Players Online:")
 	babydoll.Visit("https://letragon.ru/servers/mt/")
 }
