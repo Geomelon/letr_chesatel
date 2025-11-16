bb593ee3fb3ada4ef5eec7ce453ddbdd3770b149
Geomelon <geomelon@yandex.ru>
2025-11-16 23:17:37 +0300

    checks all servers for players, need a nice function

--- ches.go
+++ ches.go
@@ -8,15 +8,45 @@ import (
 )
 
 func main() {
-
+	emptyServer := true
 	babydoll := colly.NewCollector()
 	babydoll.UserAgent = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36")
 
-	// var users []string
 	babydoll.OnHTML(".user-server b", func(userLink *colly.HTMLElement) {
-		fmt.Println(userLink.Text)
+		fmt.Println(" ", userLink.Text)
+		emptyServer = false
 	})
 
 	fmt.Println("Players Online:")
+	fmt.Println("\nPOWER TECH")
+	babydoll.Visit("https://letragon.ru/servers/pt/")
+	if emptyServer {
+		fmt.Println("---пусто---")
+	}
+	emptyServer = true
+	fmt.Println("\nHEAVY TECH")
+	babydoll.Visit("https://letragon.ru/servers/ht/")
+	if emptyServer {
+		fmt.Println("---пусто---")
+	}
+	emptyServer = true
+	fmt.Println("\nMAGIC")
+	babydoll.Visit("https://letragon.ru/servers/m/")
+	if emptyServer {
+		fmt.Println("---пусто---")
+	}
+	emptyServer = true
+	fmt.Println("\nMISUSE TECH")
 	babydoll.Visit("https://letragon.ru/servers/mt/")
+	if emptyServer {
+		fmt.Println("---пусто---")
+	}
+	emptyServer = true
+	fmt.Println("\nNEVER CHANGE")
+	babydoll.Visit("https://letragon.ru/servers/nc/")
+	if emptyServer {
+		fmt.Println("---пусто---")
+	}
+	emptyServer = true
+
 }
